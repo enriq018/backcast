7b7829b21e94cfee8714b99e143a7d239fcd4fbd
enriq018 <enriq018coding@gmail.com>
2017-10-24 19:31:44 -0700

    add api key and data params

--- src/collections/videos.js
+++ src/collections/videos.js
@@ -8,9 +8,17 @@ var Videos = Backbone.Collection.extend({
     let data = {
       query: searchStr,
       maxResults: 5,
-      key: key,
+      key: this.key,
       
     };
+    
+    $ajax({
+      type: 'GET',
+      url: 'https://www.googleapis.com/youtube/v3/search',
+      data: data,
+      sucess: (data) => console.log('woot', JSON.stringify(data)),
+      error: (data) => console.log('failed', JSON.stringify(data))
+    });
   }
   
 });
